4adf1fa4927f726086578de5cb18be7a55e6a878
Fabien Serny <fabien@23prod.fr>
2014-11-17 00:55:57 +0100

    Add SQL table installation

---
+++ sql/install.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS `@PREFIX@ftv2_custom_menu` (
+  `id_ftv2_custom_menu` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `link` varchar(255) NOT NULL,
+  `id_employee` int(10) unsigned NOT NULL,
+  `date_add` datetime DEFAULT NULL,
+  PRIMARY KEY (`id_ftv2_custom_menu`),
+  KEY `id_employee` (`id_employee`)
+) ENGINE=@ENGINE@ DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
---
+++ sql/uninstall.sql
@@ -0,0 +1,1 @@
+DROP TABLE IF EXISTS `@PREFIX@ftv2_custom_menu`;
